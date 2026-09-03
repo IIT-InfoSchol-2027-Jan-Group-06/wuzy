20e8cebddf6ac5bccdd5e0b6f8cbb6676f4b9836
ravindu644 <droidcasts@protonmail.com>
2026-09-03 11:34:28 +0530

    Remove duplicate navigateToEvent in explore
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- frontend/app/(tabs)/explore.tsx
+++ frontend/app/(tabs)/explore.tsx
@@ -136,10 +136,6 @@ export default function ExploreScreen() {
     router.push('/event-details');
   };
 
-  const navigateToEvent = (eventId: string) => {
-    router.push('/event-details');
-  };
-
   // Filter events by category and search text
   const filteredFeaturedEvents = useMemo(() => {
     return featuredEvents.filter((event) => {

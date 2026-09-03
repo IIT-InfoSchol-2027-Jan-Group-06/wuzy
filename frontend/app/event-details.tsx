c2375b56f117498c57e42e1ed25f9436797efcce
ravindu644 <droidcasts@protonmail.com>
2026-09-03 14:43:06 +0530

    fix(event-details): drop the three dot button to match the design
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- frontend/app/event-details.tsx
+++ frontend/app/event-details.tsx
@@ -272,15 +272,7 @@ export default function EventDetailsScreen({ event }: EventDetailsScreenProps) {
           className="absolute top-0 left-0 right-0 z-10 flex-row justify-between px-[32px] pt-[49px]">
           {/* Back button - circular glass */}
           <GlassNavButton icon="arrow-back" size={backButtonSize} onPress={handleBackPress} />
-          {/* Share + More as separate circular glass buttons */}
-          <View className="flex-row items-center gap-[16px]">
-            <GlassNavButton icon="share-outline" size={backButtonSize} onPress={() => {}} />
-            <GlassNavButton
-              icon="ellipsis-horizontal-outline"
-              size={backButtonSize}
-              onPress={() => {}}
-            />
-          </View>
+          <GlassNavButton icon="share-outline" size={backButtonSize} onPress={() => {}} />
         </View>
       </SafeAreaView>
     </View>
